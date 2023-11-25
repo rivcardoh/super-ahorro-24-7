3f2a7f3ad81c16e97d858a681e23c81bb4dba24f
rivcardoh <97768577+rivcardoh@users.noreply.github.com>
2023-11-25 19:16:29 -0400

    Update ventas-247.sql

--- ventas-247.sql
+++ ventas-247.sql
@@ -1,76 +1,256 @@
-CREATE TABLE `categoria`  (
-  `id_categoria` int NOT NULL AUTO_INCREMENT,
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-11-2023 a las 00:15:35
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `super_ahorro24-7`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
   `categoria` varchar(50) NOT NULL,
-  `estado` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_categoria`)
-);
+  `descripcion` varchar(255) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `detalle_ingreso`  (
-  `id_detalle_ingreso` int NOT NULL AUTO_INCREMENT,
-  `id_ingreso` int NULL,
-  `id_producto` int NULL,
-  `cantidad` int NULL,
-  `precio_compra` decimal(11, 2) NULL,
-  `precio_venta` decimal(11, 2) NULL,
-  PRIMARY KEY (`id_detalle_ingreso`)
-);
+--
+-- Volcado de datos para la tabla `categoria`
+--
 
-CREATE TABLE `detalle_venta`  (
-  `id_detalle_venta` int NOT NULL AUTO_INCREMENT,
-  `id_venta` int NULL,
-  `id_producto` int NULL,
-  `cantidad` int NULL,
-  `precio` decimal(11, 2) NULL,
-  `descuento` decimal(11, 2) NULL,
-  PRIMARY KEY (`id_detalle_venta`)
-);
+INSERT INTO `categoria` (`id_categoria`, `categoria`, `descripcion`, `estado`) VALUES
+(1, 'Cat 1', 'De todo', 1);
 
-CREATE TABLE `ingreso`  (
-  `id_ingreso` int NOT NULL AUTO_INCREMENT,
-  `id_proveedor` int NULL,
-  `tipo_comprobante` varchar(20) NULL,
-  `nun_comprobante` varchar(10) NULL,
-  `fecha_hora` datetime NULL,
-  `impuesto` decimal(10, 2) NULL,
-  `estado` varchar(255) NULL,
-  PRIMARY KEY (`id_ingreso`)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `persona`  (
-  `id_persona` int NOT NULL AUTO_INCREMENT,
-  `tipo_persona` varchar(20) NULL,
-  `nombre` varchar(100) NULL,
-  `tipo_documento` varchar(20) NULL,
-  `num_documento` varchar(15) NULL,
-  `direccion` varchar(70) NULL,
-  `telefono` varbinary(15) NULL,
-  `email` varchar(50) NULL,
-  PRIMARY KEY (`id_persona`)
-);
+--
+-- Estructura de tabla para la tabla `detalle_ingreso`
+--
 
-CREATE TABLE `productos`  (
-  `id_producto` int NOT NULL AUTO_INCREMENT,
-  `id_categoria` int NOT NULL,
-  `codigo` varchar(50) NULL,
-  `nombre` varchar(100) NULL,
-  `stock` int NULL,
-  `descripcion` varchar(512) NULL,
-  `imagen` varchar(50) NULL,
-  `estado` varchar(20) NULL,
-  PRIMARY KEY (`id_producto`)
-);
+CREATE TABLE `detalle_ingreso` (
+  `id_detalle_ingreso` int(11) NOT NULL,
+  `id_ingreso` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precio_compra` decimal(11,2) DEFAULT NULL,
+  `precio_venta` decimal(11,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `venta`  (
-  `id_venta` int NOT NULL AUTO_INCREMENT,
-  `id_cliente` int NULL,
-  `tipo_comprobante` varchar(20) NULL,
-  `num_comprobante` varchar(10) NULL,
-  `fecha_hora` datetime NULL,
-  `impuesto` decimal(4, 2) NULL,
-  `total_venta` decimal(11, 2) NULL,
-  `estado` varchar(20) NULL,
-  PRIMARY KEY (`id_venta`)
-);
+-- --------------------------------------------------------
 
-ALTER TABLE `productos` ADD CONSTRAINT `fk_productos_productos_1` FOREIGN KEY (`id_categoria`) REFERENCES `productos` (`id_categoria`);
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
 
+CREATE TABLE `detalle_venta` (
+  `id_detalle_venta` int(11) NOT NULL,
+  `id_venta` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precio` decimal(11,2) DEFAULT NULL,
+  `descuento` decimal(11,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingreso`
+--
+
+CREATE TABLE `ingreso` (
+  `id_ingreso` int(11) NOT NULL,
+  `id_proveedor` int(11) DEFAULT NULL,
+  `tipo_comprobante` varchar(20) DEFAULT NULL,
+  `nun_comprobante` varchar(10) DEFAULT NULL,
+  `fecha_hora` datetime DEFAULT NULL,
+  `impuesto` decimal(10,2) DEFAULT NULL,
+  `estado` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `id_persona` int(11) NOT NULL,
+  `tipo_persona` varchar(20) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `tipo_documento` varchar(20) DEFAULT NULL,
+  `num_documento` varchar(15) DEFAULT NULL,
+  `direccion` varchar(70) DEFAULT NULL,
+  `telefono` varbinary(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `estado` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id_persona`, `tipo_persona`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`, `estado`) VALUES
+(1, 'cliente', 'Pedro Snchez', 'ASDAS', '11232132', 'AV pirai', 0x33322451, 'hola@hola.com', 1),
+(2, 'proveedor', 'Julia Perez', '123123s', '211', NULL, 0x33235534, 'hola@hola.com', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `codigo` varchar(50) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
+  `imagen` varchar(50) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id_venta` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `tipo_comprobante` varchar(20) DEFAULT NULL,
+  `num_comprobante` varchar(10) DEFAULT NULL,
+  `fecha_hora` datetime DEFAULT NULL,
+  `impuesto` decimal(4,2) DEFAULT NULL,
+  `total_venta` decimal(11,2) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  ADD PRIMARY KEY (`id_detalle_ingreso`);
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`id_detalle_venta`);
+
+--
+-- Indices de la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  ADD PRIMARY KEY (`id_ingreso`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`id_persona`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD UNIQUE KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id_venta`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  MODIFY `id_detalle_ingreso` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  MODIFY `id_detalle_venta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  MODIFY `id_ingreso` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `id_categoria_producto` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
